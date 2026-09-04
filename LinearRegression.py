220597d4a97d0bb36f2c18d0ffdc9f53c9367c2a
Komal799921 <komaldushing2007@gmail.com>
2026-09-04 11:38:37 +0530

    Add files via upload

--- LinearRegression.py
+++ LinearRegression.py
@@ -57,3 +57,43 @@ plt.ylabel("Predicted Total Amount")
 plt.title("Actual vs Predicted Total Amount")
 plt.grid(True)
 plt.show()
+
+
+
+"""import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+df = pd.read_csv("Retail_Sales_Dataset.csv")
+
+print(df.head())
+print(df.info())
+print(df.tail())
+
+# Input features
+X = df[["Age", "Quantity", "Price per Unit"]]
+
+# Target
+y = df["Total Amount"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Create model
+model = LinearRegression()
+
+# Train model
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Accuracy metrics
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
+
+# Predict new value
+print("Prediction:", model.predict([[25, 3, 100]]))"""
